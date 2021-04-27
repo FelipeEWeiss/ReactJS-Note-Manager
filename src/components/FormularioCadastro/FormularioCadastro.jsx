343e5a10bac9d44d309c61dea0b643a875d24b66
FelipeWeiss <felipeew19@gmail.com>
2021-04-26 21:18:32 -0300

    Projeto Parte 3

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -6,8 +6,28 @@ class FormularioCadastro extends Component {
     super();
     this.titulo = "";
     this.texto = "";
+    this.categoria = "";
+    this.state = {categorias:[]}
+
+    this._novasCategorias = this._novasCategorias.bind(this);
   }
-  
+  componentDidMount(){
+    this.props.categorias.inscrever(this._novasCategorias);
+  }
+
+  componentWillUnmount(){
+    this.props.categorias.desinscrever(this._novasCategorias);
+  }
+
+  _novasCategorias(categorias){
+    this.setState({...this.state, categorias})
+  }
+
+  _handleMudancaCategoria(evento){
+    evento.stopPropagation();
+    this.categoria = evento.target.value;
+  }
+
   _handleMudancaTitulo(evento){
     evento.stopPropagation();
     this.titulo = evento.target.value
@@ -21,14 +41,23 @@ class FormularioCadastro extends Component {
   _criarNota(evento){
     evento.preventDefault();
     evento.stopPropagation();
-    this.props.criarNota(this.titulo, this.texto);
+    this.props.criarNota(this.titulo, this.texto, this.categoria);
   }
 
   render() {
     return (
-      <form className="form-cadastro "
+      <form className="form-cadastro"
         onSubmit={this._criarNota.bind(this)}
       >
+        <select 
+          onChange={this._handleMudancaCategoria.bind(this)} 
+          className="form-cadastro_input"
+        >
+          <option>Sem categoria</option>
+          {this.state.categorias.map((categoria, index) => {
+            return <option key={index}>{categoria}</option>
+          })}
+        </select>
         <input
           type="text"
           placeholder="Título"
